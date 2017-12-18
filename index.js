c5ba610814f505f0ed1150340b24f282b08dab5a
Alexander Praetorius <dev@serapath.de>
2017-12-18 23:48:44 +0700

    add second draft

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ var minixhr = require('minixhr')
 var csjs = require('csjs-inject')
 var bel = require('bel')
 
-console.info('Hello World')
+console.info('Hello WorkShopper')
+console.log('Learn yourself how to')
+console.error('BUY BITCOINS')
